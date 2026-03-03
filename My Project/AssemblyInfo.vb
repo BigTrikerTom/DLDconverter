c723306a348fb57bce8ceab5d82fb284ed0a3b9c
BigTrikerTom <ts@thomas-steger.com>
2026-03-03 21:52:42 +0100

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("DLDconverter")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("TimeShareIt GdbR")>
+<Assembly: AssemblyProduct("DLDconverter")>
+<Assembly: AssemblyCopyright("Copyright © TimeShareIt GdbR 2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7c373c2c-9b57-4b95-bcbe-b7b5fb4d61ff")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
